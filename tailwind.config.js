d7254570a1838076a915d3c67bafa8d28065bb71
fabrimetalsa <jaguilera@fabrimetalsa.cl>
2024-09-23 15:13:46 -0300

    DB y Schema de Controladores

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         './resources/js/*.vue',
         './resources/js/components/**',
     ],
-
+    darkMode: 'selector',
     theme: {
         extend: {
             fontFamily: {
