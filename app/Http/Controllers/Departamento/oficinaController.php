5583813c9d866ec5067cbba4e476170d403ee7a7
fabrimetalsa <jaguilera@fabrimetalsa.cl>
2024-09-24 17:23:27 -0300

    CRUD Oficina y Env(Email) Listo

--- app/Http/Controllers/Departamento/oficinaController.php
+++ app/Http/Controllers/Departamento/oficinaController.php
@@ -4,62 +4,112 @@ namespace App\Http\Controllers\Departamento;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Departamento\Oficina;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 
 class oficinaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        try{
+            $oficina = Oficina::paginate();
+            return response()->json([
+                'status' => true,
+                'message' => 'Listado de todas las Oficina!',
+                'data' => $oficina
+            ], 200);
+
+        }catch(ModelNotFoundException $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'No se ha encontrado las Oficina',
+            ], 404);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Ha ocurrido algun error al realizar la petición!',
+            ], 500);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try{
+            $oficina = Oficina::create($request->all());
+            return response()->json([
+                'status' => true,
+                'message' => 'Se ha registrado con exito!',
+                'data' => $oficina
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Ha ocurrido algun error al realizar la petición!',
+            ], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        try{
+            $oficina = Oficina::findOrFail();
+            return response()->json([
+                'status' => true,
+                'message' => 'Listado de todas las Oficina!',
+                'data' => $oficina
+            ], 200);
+
+        }catch(ModelNotFoundException $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'No se ha encontra la Oficina seleccionada!',
+            ], 404);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Ha ocurrido algun error al realizar la petición!',
+            ], 500);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        try{
+            $oficina = Oficina::find($id);
+            $oficina->update($request->all());
+        }catch(Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Ha ocurrido algun error al realizar la petición!',
+            ], 500);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        try{
+            $oficina = Oficina::find($id);
+            $oficina->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Se ha eliminado con exito!',
+            ], 200);
+        }catch(ModelNotFoundException $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'No se ha encontra la Oficina seleccionada!',
+            ], 404);
+        }catch(QueryException $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'No se puede eliminar el Computador porque tiene dependencias.',
+            ], 409);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Ha ocurrido algun error al realizar la petición!',
+            ], 500);
+        }
     }
 }
